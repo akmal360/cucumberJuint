a3494600bc8cd20fdc01693fb8b77fee94dbc418
akmal360 <akmalcode@hotmail.com>
2022-10-07 17:15:10 -0400

    first commit

---
+++ CucumberJunit/src/test/java/stepDefination/SearchSteps.java
@@ -0,0 +1,38 @@
+package stepDefination;
+
+import amazonProducts.Product;
+import amazonProducts.Search;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+
+
+public class SearchSteps {
+
+	Product product;
+	Search search;
+	
+	@Given("I want to write a step with search on Amazon page")
+	public void i_want_to_write_a_step_with_search_on_amazon_page() {
+		System.out.println("Step 1");
+		
+	}
+	@When("I search for product with {string} and price is {int}")
+	public void i_search_for_product_with_and_price_is(String prodectName, Integer price) {
+		System.out.println("Step 2: ProductName: "+prodectName+"Price"+price);
+		
+		product =new Product(prodectName, price);
+	}
+
+	@Then("product with name {string} should displayed")
+	public void product_with_name_should_displayed(String prodectName) {
+		System.out.println("Step 3: ProductName:"+prodectName);
+		
+		search=new Search();
+		String nameProduct=  search.displayProduct(product);
+		System.out.println("Searched product name"+nameProduct);
+		
+	
+	}
+}
---
+++ CucumberJunit/src/test/java/testRunners/AmazonRunner.java
@@ -0,0 +1,11 @@
+package testRunners;
+
+
+
+public class AmazonRunner {
+	
+	int i=04;
+	int l=04;
+	int k=033;
+}
+
